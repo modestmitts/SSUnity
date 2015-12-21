61e4452bf61533adc63e2e8c0068c6d75e8b3897
RogerNattkemper <roger.nattkemper@gmail.com>
2015-12-21 13:23:57 -0800

    Git Wrench Work

--- SS Unity/Assets/Scripts/PowerGridInventory/MyItemFilter.cs
+++ SS Unity/Assets/Scripts/PowerGridInventory/MyItemFilter.cs
@@ -9,16 +9,15 @@ public class MyItemFilter : MonoBehaviour
 
     public void IsTypeValid(PGISlotItem item, PGIModel model, PGISlot slot)
     {
-        print("IN IsTypeValid");
+
         if (AllowedTypes != null && AllowedTypes.Count > 0)
         {
             
-            model.CanPerformAction = true;
-
+          
             var type = item.GetComponent<MyItemType>();
             if (type != null && AllowedTypes.Contains(type.Type))
             {
-                print("In the good return");
+                
                 return;
             }
 
---
+++ SS Unity/Assets/Scripts/PowerGridInventory/MyTrashcan.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using PowerGridInventory;
+
+public class MyTrashcan : MonoBehaviour 
+{
+    public void TrashItem(PGISlotItem item, PGIModel inv, PGISlot slot)
+    {
+        //First, remove this item from the equipement slot
+        inv.Unequip(item);
+
+        //Next, trigger removal events on the item.
+        item.TriggerCanUnequipEvents(inv, slot);
+        item.TriggerRemoveEvents(inv);
+    }
+  
+    
+
+}
