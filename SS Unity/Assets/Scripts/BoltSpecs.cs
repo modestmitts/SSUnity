637cb80fb8b0ccff36769a6fa82d1a5c3d05ab10
RogerNattkemper <roger.nattkemper@gmail.com>
2016-01-09 14:39:38 -0800

    Git New DL'd Assets, FPS Hangar, Toolbox Moved DL'd Assets to own directory "Downloaded Assets" and moved them into it (Hence all the "renamed: " entries below) Blender created a new integrated plate with bolts for testing

---
+++ SS Unity/Assets/Scripts/BoltSpecs.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltSpecs : MonoBehaviour 
+{
+    
+    
+    public float Travel = 0.15f; // How far the bolt has to travel until tight (from loose)
+    public float ySpeed = .0001f; // The speed at which the bolt goes in or out.
+    public bool Loose; // Is the bolt loose at the beginning
+    public float Turned; // How far the bolt has turned
+
+    public float LoosePos; // Where the bolt starts
+    public float TightPos; // The tight position
+    public float Current; //Current z position
+    
+
+    void Awake()
+    {
+        //All bolts (Fasteners) should start either completely loose, or all the way tight
+        if (Loose)
+        {
+            LoosePos = this.transform.localPosition.y;
+            TightPos = LoosePos - Travel;
+            Current = LoosePos;
+        }
+        else
+        {
+            TightPos = this.transform.localPosition.y;
+            LoosePos = TightPos + Travel;
+            Current = TightPos;
+        }
+
+        Turned = 0.0f;
+    }
+
+    public void TurnBolt(bool direction, float turnSpeed)
+    {
+        Vector3 yS = (direction) ? new Vector3(0, -ySpeed, 0) : new Vector3(0, ySpeed, 0);
+        
+        // If tighten and it's not already tight, or loosen and it's not already loose
+        if (((direction) && (Current >= TightPos)) || ((!direction) && (Current <= LoosePos)))
+        {
+            //If 
+            this.transform.localPosition += yS;
+            this.transform.Rotate(0, 0, turnSpeed);
+            Current = this.transform.localPosition.y;
+            Turned += turnSpeed;
+            if ((Turned >= 360) || (Turned <= -360)) Turned = 0;
+            if (Current >= LoosePos) Loose = true;
+            else Loose = false;
+        }       
+    }
+}
--- SS Unity/Assets/Scripts/engageWrench.cs
+++ SS Unity/Assets/Scripts/engageWrench.cs
@@ -13,11 +13,26 @@ public class engageWrench : MonoBehaviour
     //Rotation(0, 90,  0)
     //Scale (0.0333, 0.0333, 0.0333)
     public GameObject wrench;
-    private Vector3 BoltAdjust = new Vector3(-0.012f, -0.075f, 0.0075f);
-    private Vector3 PlayerAdjust = new Vector3(0.283f, 0.386f, 0.64f);
-        
+    public float turnSpeed = 0.15f;
+
+    private Vector3[] wrenchPosition = new[] { new Vector3(-0.012f, -0.075f, 0.006f), // Position0 (Tight)
+                                                new Vector3(0.0565f, -0.05f, 0.006f), // Position1 (Loose)
+                                                new Vector3(0.07f, 0.027f, 0.006f), // Position2  
+                                                new Vector3(0.012f, 0.075f, 0.006f), // Position3 
+                                                new Vector3(-0.0565f, 0.05f, 0.006f), // Position4 
+                                                new Vector3(-0.07f, -0.027f, 0.006f) }; // Position5
+
+    private float [] wrenchTurn = new[]{0.0f, -60.0f, -120.0f, -180.0f, -240.0f, -300.0f};
+    
+    private Vector3 PlayerAdjust = new Vector3(0.283f, 0.386f, 0.64f); 
+   
+   
 
     private bool adjusted = false;
+    private bool ready = false;
+    private BoltSpecs boltstats;
+    private int currentPos;
+
     public void wrenchBolt(GameObject newParent, bool work)
     {
         
@@ -25,13 +40,19 @@ public class engageWrench : MonoBehaviour
         {
             if (!adjusted)
             {
+                boltstats = transform.parent.GetComponent(typeof(BoltSpecs)) as BoltSpecs; 
                 transform.parent = newParent.transform;
-                this.transform.localPosition = Vector3.zero;
-                this.transform.localPosition = BoltAdjust;
+                this.transform.localPosition = Vector3.zero;            
+
+                if (!boltstats.Loose) currentPos = 0;
+                else currentPos = 1;
+                
+                this.transform.localRotation = Quaternion.Euler(wrenchTurn[currentPos], 90.0f, 0.0f);
+                this.transform.localPosition = wrenchPosition[currentPos];
                 wrench.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
-                this.transform.localRotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
                 wrench.transform.parent = this.transform;
-                wrench.transform.position = this.transform.position;
+                wrench.transform.position = this.transform.position;                
+                ready = true;
                 adjusted = true;
             }
 
@@ -40,6 +61,8 @@ public class engageWrench : MonoBehaviour
         {
             if (adjusted)
             {
+                ready = false;
+                boltstats = null;
                 transform.parent = newParent.transform;
                 this.transform.localPosition = Vector3.zero;
                 this.transform.localPosition = PlayerAdjust;
@@ -47,11 +70,30 @@ public class engageWrench : MonoBehaviour
                 this.transform.localRotation = Quaternion.Euler(0.0f, -34.0f, 0.0f);
                 wrench.transform.parent = this.transform;
                 wrench.transform.position = this.transform.position;   
-                adjusted = false;
+                adjusted = false;                
             }
         }
         
     }
 
-
+    void Update()
+    {
+        if (ready)
+        {
+            bool tighten = true;
+            // If Right Button Down
+            if (Input.GetMouseButton(1)) boltstats.TurnBolt(tighten, turnSpeed);                               
+            if (Input.GetMouseButton(0)) boltstats.TurnBolt(!tighten, -turnSpeed);           
+   
+            if (((int)boltstats.Turned / 60) != currentPos) 
+            {
+               currentPos = Mathf.Abs((int)boltstats.Turned / 60);
+               Debug.Log("Current Position" + currentPos);
+               this.transform.localRotation = Quaternion.Euler(wrenchTurn[currentPos], 90.0f, 0.0f);
+               this.transform.localPosition = wrenchPosition[currentPos];
+               currentPos = 1;
+            }
+        }    
+    }
+    
 }
